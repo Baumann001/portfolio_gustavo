1355c6de8883acb182ee9a4204ab7bd1f72e00e6
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-10-25 12:12:09 -0300

    ad---

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+const navbar = document.querySelector('.navbar');
+const mobileNavbar = document.querySelector('.navbar__mobile');
+const button = document.querySelector('.burguer');
+
+button.addEventListener('click', function () {
+  mobileNavbar.classList.toggle('active');
+});
+
+window.addEventListener('scroll', function () {
+  if (this.window.pageYOffset > 0) return navbar.classList.add('active');
+  return navbar.classList.remove('active');
+});
+
+// Dark mode toggle
 let trilho = document.getElementById('trilho');
 let body = document.querySelector('body');
 
-
-
-trilho.addEventListener('click',()=>{
-    trilho.classList.toggle('dark');
-    body.classList.toggle('dark');
-})
+trilho.addEventListener('click', () => {
+  trilho.classList.toggle('dark');
+  body.classList.toggle('dark');
+});
